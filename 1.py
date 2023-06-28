5af33ac783192d55939934786b4682ae1829aec6
mariianosova <122862357+mariianosova@users.noreply.github.com>
2023-06-29 00:10:20 +0400

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,11 +1,17 @@
+import unittest
 
-def format(input_file, output_file, line_length):
-    with open(input_file, 'r') as file:
+def read_data(file_name):
+    with open(file_name, 'r') as file:
         lines = file.readlines()
+    return lines
 
+def write_data(file_name, data):
+    with open(file_name, 'w') as file:
+        file.write("\n".join(data))
+
+def get_format(data, line_length):
     format_lines = []
-
-    for line in lines:
+    for line in data:
         words = line.strip().split()
         format_line = ""
         current_length = 0
@@ -19,14 +25,42 @@ def format(input_file, output_file, line_length):
                 format_lines.append(format_line.strip())
                 format_line = word + " "
                 current_length = word_length + 1
-                
+
         format_lines.append(format_line.strip())
 
-    with open(output_file, 'w') as file:
-        file.write("\n".join(format_lines))
+    return format_lines
 
-input_file = input("имя входного файла ")
-output_file = input("имя результирующего файла ")
-line_length = int(input("длина строки "))
+def main():
+    input_file = input("имя входного файла ")
+    output_file = input("имя результирующего файла ")
+    line_length = int(input("длина строки "))
 
-format(input_file, output_file, line_length)
+    data = read_data(input_file)
+    result = get_format(data, line_length)
+    write_data(output_file, result)
+
+class TestDataFormatting(unittest.TestCase):
+    def test_get_format(self):
+        data = ["Hello How", "Are you from?", "I'm from good", "Thanks"]
+        line_length = 10
+
+        expected_result = ["Hello How", "Are you", "from?", "I'm from", "good", "Thanks"]
+        result = get_format(data, line_length)
+
+        self.assertEqual(result, expected_result)
+
+    def test_write_data(self):
+        file_name = "output.txt"
+        data = ["Hello How", "Are you", "from?", "I'm from", "good", "Thanks"]
+
+        write_data(file_name, data)
+
+        with open(file_name, 'r') as file:
+            lines = file.readlines()
+
+        result = [line.strip() for line in lines]
+
+        self.assertEqual(result, data)
+
+if __name__ == '__main__':
+    unittest.main()
